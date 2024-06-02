0367c2d03e39f6892612e8a244ac82289252a239
Demi Zheng <27demiz@students.harker.org>
2024-06-01 17:51:34 -0700

    changed title and description for metadata

--- app/page.tsx
+++ app/page.tsx
@@ -2,30 +2,34 @@
 import React from 'react';
 import LightButton from '../components/LightButton';
 import DarkButton from '../components/DarkButton';
+import Head from 'next/head';
 
 const HomePage: React.FC = () => {
   return (
-
-    <div className="min-h-screen bg-beige flex flex-col items-center">
-      <nav className="w-full flex justify-end p-6 space-x-4">
-        <LightButton text="Log In" link="/login" style="bg-beige"/>
-        <DarkButton text="Sign Up" link="/signup" style="ml-4"/>
-      </nav>
-      <div className="mt-24 text-center">
-        <h1 className="text-5xl font-bold mb-4">certamen.</h1>
-        <p className="text-lg text-gray-700 mb-2">
-          pursue your studies of the classics
-        </p>
-        <p className="text-lg text-gray-700 mb-8 italic">
-          discite ludendo :) 
-        </p>
-        <div className="flex space-x-4 justify-center">
-          <DarkButton text="Study" link="/study" />
-          <LightButton text="Play" link="/play" />
+    <>
+      <Head>
+        <title> certamen. </title>
+      </Head>
+      <div className="min-h-screen bg-beige flex flex-col items-center">
+        <nav className="w-full flex justify-end p-6 space-x-4">
+          <LightButton text="Log In" link="/login" style="bg-beige"/>
+          <DarkButton text="Sign Up" link="/signup" style="ml-4"/>
+        </nav>
+        <div className="mt-24 text-center">
+          <h1 className="text-5xl font-bold mb-4">certamen.</h1>
+          <p className="text-lg text-gray-700 mb-2">
+            pursue your studies of the classics
+          </p>
+          <p className="text-lg text-gray-700 mb-8 italic">
+            discite ludendo :) 
+          </p>
+          <div className="flex space-x-4 justify-center">
+            <DarkButton text="Study" link="/study" />
+            <LightButton text="Play" link="/play" />
+          </div>
         </div>
       </div>
-    </div>
-
+    </>
   );
 };
 
