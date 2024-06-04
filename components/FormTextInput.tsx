9c1aeb112d2655c94b81195e9ca506285fa647ed
Demi Zheng <demizhn@gmail.com>
2024-06-03 23:43:28 -0700

    d20: type of input made ambiguous

--- components/FormTextInput.tsx
+++ components/FormTextInput.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 interface InputProps {
   label?: string;
-  type: string;
+  type?: string;
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   id: string;
   required?: boolean;
 }
 
-const Input: React.FC<InputProps> = ({ label, type, value, onChange, id, required=false }) => (
+const Input: React.FC<InputProps> = ({ label, type='text', value, onChange, id, required=false }) => (
   <div className="mb-4">
     {!(label == null) && <label htmlFor={id} className="block text-gray-700 text-xs font-bold mb-2 uppercase">
       {label}
