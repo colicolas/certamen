19a8b50bb512ee3bd8721fabede4bd3caebcdb7b
Demi Zheng <demizhn@gmail.com>
2024-06-04 12:39:25 -0700

    d23: fixed cannot read properties of undefined in checkEmail\route.ts

--- app/api/auth/checkEmail/route.ts
+++ app/api/auth/checkEmail/route.ts
@@ -5,8 +5,25 @@ export async function POST(req: NextRequest) {
   const body = await req.json();
   const { email } = body;
 
-  const emailRef = await db.collection('users').where('email', '==', email).get();
+
+  if (!email) {
+    return NextResponse.json({ message: 'Email is required' }, { status: 400 });
+  }
+
+  try {
+    const userRef = await db.collection('users').where('email', '==', email).get();
+
+    if (!userRef.empty) {
+      return NextResponse.json({ exists: true }, { status: 200 });
+    } else {
+      return NextResponse.json({ exists: false }, { status: 200 });
+    }
+  } catch (error) {
+    console.error('Error checking email:', error);
+    return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+  }
+  /*const emailRef = await db.collection('users').where('email', '==', email).get();
   const exists = !emailRef.empty;
 
-  return NextResponse.json({ exists });
+  return NextResponse.json({ exists });*/
 }
