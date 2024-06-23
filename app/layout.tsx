22343e5305bbf871e8e6d63281c7106f5ef65f98
demicrat <demizhn@gmail.com>
2024-06-23 12:15:47 -0700

    fixed deploy issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     <>
       <html lang="en">
         <head>
-          <meta charset="UTF-8"/>
+          <meta charSet="UTF-8"/>
           <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
           <meta property="og:title" content="certamen."/>
